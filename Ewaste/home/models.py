369bcc27edfe1a824ea9c96f76e25c9bab982b75
diya1511 <diyaagrawal1511@gmail.com>
2021-09-21 18:10:59 +0530

    .

---
+++ Ewaste/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField(max_length=120)
+    phone=models.CharField(max_length=120)
+    desc=models.TextField()
+    date=models.DateField()
+    address=models.TextField()
+    
+    def __str__(self):
+       return self.name
+    
---
+++ Ewaste/home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from django.http.response import HttpResponseNotAllowed
+from django.http import HttpResponse
+from .models import *
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.auth.models import auth , User 
+from django.contrib.auth  import authenticate,  login, logout
+def home(request):
+    return render(request,"index.html")
+ 
+def about(request) :
+    return render(request,"about.html")
+def contact(request) :
+    if request.method == 'POST' :
+      name = request.POST.get('name')
+      email= request.POST.get('email')
+      phone = request.POST.get('phone')
+      desc = request.POST.get('desc')
+      address = request.POST.get('address')
+      contact =Contact(name=name,email=email,phone=phone,desc=desc,address=address,date= datetime.today())
+      contact.save()
+      messages.success(request, 'Your message has been sent!')
+    return render(request,"contact.html")
+
+def services(request) :
+    return render(request,"services.html")
