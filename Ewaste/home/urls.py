d03f5081a81061132b21db0223b8d2bff4595ff6
diya1511 <diyaagrawal1511@gmail.com>
2021-09-22 23:26:00 +0530

    changes

--- Ewaste/home/urls.py
+++ Ewaste/home/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("home/",views.home, name='home') ,
     path("about/",views.about, name='about') ,
     path("contact/",views.contact, name='contact') ,
+    path("services/",views.services, name='services') ,
     path('login/' , views.login , name = 'login'),
    
 ]
